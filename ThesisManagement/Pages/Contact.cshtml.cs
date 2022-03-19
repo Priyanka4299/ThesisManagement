9a1c9b675f9f315b4df23e7577a4008ab4cf47ee
JayrajGoyal7 <98147197+JayrajGoyal7@users.noreply.github.com>
2022-03-19 16:41:47 +0530

    Final feel of the UI.

---
+++ ThesisManagement/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThesisManagement.Pages.Shared
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
